b26c3fccbf9272391e5fb569c4588b59400fb52e
Ingo Griebsch <ingo.griebsch@gmx.de>
2018-10-19 22:01:41 +0200

    refactor: replace startup class with spring datasource initialize

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'Africa', 30370000, 1287920000);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'Antarctica', 14000000, 4490);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'Asia', 44579000, 4545133000);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'Australia', 8600000, 41261000);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'Europe', 10180000, 742648000);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'North America', 24709000, 587615000);
+INSERT INTO continent (id, name, area, population) VALUES (RANDOM_UUID(), 'South America', 17840000, 428240000);
