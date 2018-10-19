8039c0361c820c945870c4100cc2a05d8d832571
Ingo Griebsch <ingo.griebsch@gmx.de>
2018-10-19 18:46:44 +0200

    feat: introduce repository to CRUD continents

--- src/main/java/com/github/ingogriebsch/sample/spring/boot/graphql/model/Continent.java
+++ src/main/java/com/github/ingogriebsch/sample/spring/boot/graphql/model/Continent.java
@@ -1,9 +1,30 @@
 package com.github.ingogriebsch.sample.spring.boot.graphql.model;
 
-import lombok.Value;
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PRIVATE;
 
-@Value
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor(access = PACKAGE)
 public class Continent {
 
+    @Id
+    @Setter(PRIVATE)
     private String name;
+    private Integer area;
+    private Long population;
+
+    public Continent(@NonNull String name) {
+        this.name = name;
+    }
 }
