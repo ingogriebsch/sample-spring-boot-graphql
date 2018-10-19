2dce51fa52f43cb52bf209db901faa8797dab7c9
Ingo Griebsch <ingo.griebsch@gmx.de>
2018-10-19 19:48:57 +0200

    feat: obtain continents from repository

--- src/main/java/com/github/ingogriebsch/sample/spring/boot/graphql/resolver/Query.java
+++ src/main/java/com/github/ingogriebsch/sample/spring/boot/graphql/resolver/Query.java
@@ -1,19 +1,23 @@
 package com.github.ingogriebsch.sample.spring.boot.graphql.resolver;
 
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.github.ingogriebsch.sample.spring.boot.graphql.model.Continent;
+import com.github.ingogriebsch.sample.spring.boot.graphql.repository.ContinentRepository;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class Query implements GraphQLQueryResolver {
 
-    public List<Continent> continents() {
-        return newArrayList(new Continent("Africa"));
+    @NonNull
+    private final ContinentRepository continentRepository;
+
+    public Iterable<Continent> continents() {
+        return continentRepository.findAll();
     }
 
 }
